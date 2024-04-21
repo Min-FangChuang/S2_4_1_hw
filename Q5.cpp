8027cba2025b87086b14fca16f7078b6a33cb6cb
minfang <a0984308118@gmail.com>
2024-04-21 18:14:26 +0800

    <style> formating the project Q5.cpp and Q6.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,7 +1,13 @@
+/*Programming: Min-Fang Chuang   Date: 2024/04/15
+ *Instruction: Chow-Sing Lin     Class: Program Design
+ *
+ *Define a class HotDogStand
+ */
 #include<iostream>
 #include<cstdlib>
 using namespace std;
 
+//define HotDogStand
 class HotDogStand {
 public:
 	HotDogStand(int Id, int Sold);
@@ -17,16 +23,21 @@ private:
 
 int HotDogStand::totalSold = 0;
 int main() {
+	//create the object
 	HotDogStand hds1(1, 8), hds2(2, 243), hds3(3, 323), hds4;
+
+	//display the condition of sold
 	cout << "The total sold of hot dog stands " << HotDogStand::getTotalSold() << endl;
 	cout << "(" << hds1.getSold() << "+" << hds2.getSold() << "+" << hds3.getSold() << "+" << hds4.getSold() << ")" << endl;
 
+	//sale some hot dog
 	hds1.JustSold();
 	hds1.JustSold();
 	hds2.JustSold();
 	hds1.JustSold();
 	hds4.JustSold();
 
+	//display the condition of sold again
 	cout << "\n<One hour later>" << endl;
 	cout << "The total sold of hot dog stands " << HotDogStand::getTotalSold() << endl;
 	cout << "(" << hds1.getSold() << "+" << hds2.getSold() << "+" << hds3.getSold() << "+" << hds4.getSold() << ")" << endl;
@@ -35,10 +46,13 @@ int main() {
 	return(0);
 }
 
+//constructor
 HotDogStand::HotDogStand(int Id, int Sold) : id(Id),sold(Sold){
 	totalSold += sold;
 }
 HotDogStand::HotDogStand() : id(0), sold(0) {}
+
+//increments the number of hot dogs the stand has sold by one
 void HotDogStand::JustSold() {
 	sold++;
 	totalSold++;
--- Q6.cpp
+++ Q6.cpp
@@ -1,28 +1,37 @@
+/*Programming: Min-Fang Chuang   Date: 2024/04/15
+ *Instruction: Chow-Sing Lin     Class: Program Design
+ *
+ *store each student's grade by vector and output a histogramof grades
+ */
 #include<iostream>
 #include<cstdlib>
 #include<vector>
 using namespace std;
 
 int main() {
+	//input - scores
 	vector<int>score;
 	int Score, Max = -1;
 	cout << "Enter student's grades (use '-1' as the end): " << endl;
 	while (true) {
 		cin >> Score;
-		if (Score == -1) {
+		if (Score == -1) {   //enter until -1
 			break;
 		}
 		score.push_back(Score);
-		if (Score > Max) { Max = Score; }
+		if (Score > Max) { Max = Score; }  //find the maximum score
 	}
 
+	//output - the histogram of grades
 	vector<int>histogram;
 	histogram.resize(Max + 1, 0);
 
+	//compute the histogram
 	for (int i = 0;i < score.size();i++) {
 		histogram[score[i]]++;
 	}
 
+	//display the histogram
 	cout << "\nHistogram of grades" << endl;
 	for (int i = 0;i <= Max;i++) {
 		cout << i << " ";
